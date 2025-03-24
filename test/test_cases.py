003e848d618bf84f57dc99f974beaeb437ed1225
Saket Rajak <saketsince05@gmail.com>
2025-03-24 23:33:09 +0530

    1

--- test/test_cases.py
+++ test/test_cases.py
@@ -13,5 +13,5 @@ detector.graph.add_edge("P1", "R1")
 detector.graph.add_edge("R1", "P2")
 detector.graph.add_edge("P2", "R2")
 detector.graph.add_edge("R2", "P1")  # Circular wait
- 
+
 detector.run()
